d0ad50967c5c2f4989517d16f0b5f64811f409e7
TranManhThang <tranmanhthang96@gmail.com>
2024-01-02 17:08:03 +0700

    fetching data

--- components/Meals/MealsGrid.tsx
+++ components/Meals/MealsGrid.tsx
@@ -3,7 +3,7 @@ import classes from './meals-grid.module.css';
 
 export default function MealsGrid({ meals }: { meals: IMealItemProps[] }) {
   return (
-    <ul>
+    <ul className={classes.meals}>
       {meals.map((meal: IMealItemProps) => (
         <li key={meal.id}>
           <MealItem {...meal} />
